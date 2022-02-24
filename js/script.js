f03a60880e6dd79ce6d7c3246d441276cce416ea
Marcos Barcelos <marcosbarcelosdev@gmail.com>
2022-02-23 23:43:02 -0300

    Versão final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const ButtonDropdown = document.getElementById('btn-dropdown');
-const ButtonMobile = document.getElementById ('btn-mobile');
+const ButtonMobile = document.getElementById('btn-mobile');
 
 function DropdownMenu() {
     let Dropdown = document.getElementById('dropdown');
@@ -10,21 +10,21 @@ function DropdownMenu() {
         icon.classList.remove('bi-chevron-down')
         icon.classList.add('bi-chevron-up')
 
-    }else {
+    } else {
         Dropdown.style.display = "none";
         icon.classList.remove('bi-chevron-up')
         icon.classList.add('bi-chevron-down')
     }
 }
 
-function MobileMenu () {
-   let Container = document.getElementById('menu-container');
-   let IconHamb = document.getElementById('hamb');
+function MobileMenu() {
+    let Container = document.getElementById('menu-container');
+    let IconHamb = document.getElementById('hamb');
 
-   if (Container.style.display === "none") {
-       Container.style.display = "block";
+    if (Container.style.display === "none") {
+        Container.style.display = "block";
 
-    }else {
+    } else {
         Container.style.display = "none";
     }
 }
